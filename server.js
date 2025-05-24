297f48b411cfe2454d4e415750e4b744a9ecfa8a
នʜɪᴠᴀᴍ <shivam334488@gmail.com>
2025-05-24 16:04:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-const telegramToken = 'YOUR_TELEGRAM_BOT_TOKEN';
-const telegramChatId = 'YOUR_TELEGRAM_CHAT_ID';
+const telegramToken = '6298320462:AAFgjEjGy0udiRNZRG1VVpooKYyQpj4UF6U';
+const telegramChatId = '6000036430';
 app.post('/location', (req, res) => {
   // code here
 });
